4142e236b2830e61128d9e160592f8c32a8e929e
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2026-01-27 16:16:07 -0300

    Adicionar Usuario

--- src/main/java/br/com/sandro/projeto/model/Usuario.java
+++ src/main/java/br/com/sandro/projeto/model/Usuario.java
@@ -1,4 +1,11 @@
 package br.com.sandro.projeto.model;
 
 public class Usuario {
+    private Long id;
+    private String nome;
+    private int quantidadeHoras;
+    private boolean participou;
+    private String observacao;
+
+    // getters e setters
 }
