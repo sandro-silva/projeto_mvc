c94e440d47362f57e62b788e6771e903fe65bfb6
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2026-01-27 16:24:22 -0300

    Adicionar UsuarioView

--- src/main/java/br/com/sandro/projeto/view/UsuarioView.java
+++ src/main/java/br/com/sandro/projeto/view/UsuarioView.java
@@ -1,4 +1,16 @@
 package br.com.sandro.projeto.view;
 
+import br.com.sandro.projeto.controller.UsuarioController;
+import java.util.Scanner;
+
 public class UsuarioView {
+    private UsuarioController controller = new UsuarioController();
+    private Scanner sc = new Scanner(System.in);
+
+    public void menu() {
+        System.out.println("1 - Cadastrar");
+        System.out.println("2 - Listar");
+        System.out.println("3 - Atualizar");
+        System.out.println("4 - Deletar");
+    }
 }
