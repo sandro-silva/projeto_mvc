11eb20f2eb95cf70c880a4fc0db14350219fad57
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2026-01-27 16:19:44 -0300

    Adicionar UsuarioController

--- src/main/java/br/com/sandro/projeto/controller/UsuarioController.java
+++ src/main/java/br/com/sandro/projeto/controller/UsuarioController.java
@@ -1,4 +1,21 @@
 package br.com.sandro.projeto.controller;
 
 public class UsuarioController {
+    private UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+    public void salvar(Usuario usuario) {
+        usuarioDAO.salvar(usuario);
+    }
+
+    public List<Usuario> listar() {
+        return usuarioDAO.listar();
+    }
+
+    public void atualizar(Usuario usuario) {
+        usuarioDAO.atualizar(usuario);
+    }
+
+    public void deletar(Long id) {
+        usuarioDAO.deletar(id);
+    }
 }
