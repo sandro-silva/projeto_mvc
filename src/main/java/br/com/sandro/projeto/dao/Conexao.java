e7af7f407f67c6773965ce0d31ecd2a06425546c
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2026-01-27 16:47:52 -0300

    Adicionar Conexao

--- src/main/java/br/com/sandro/projeto/dao/Conexao.java
+++ src/main/java/br/com/sandro/projeto/dao/Conexao.java
@@ -1,4 +1,17 @@
 package br.com.sandro.projeto.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/projeto_mvc";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "senha";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
