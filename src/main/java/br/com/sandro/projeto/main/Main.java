e43b0baec99fae7017092dc8099febedeb53260e
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2026-01-27 17:00:46 -0300

    Adicionar Main

---
+++ src/main/java/br/com/sandro/projeto/main/Main.java
@@ -0,0 +1,9 @@
+package br.com.sandro.projeto.main;
+
+import br.com.sandro.projeto.view.UsuarioView;
+
+public class Main {
+    public static void main(String[] args) {
+        new UsuarioView().iniciar();
+    }
+}
